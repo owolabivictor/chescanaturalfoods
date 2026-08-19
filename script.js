5799a859f341c4d230c69f3228f23dc599bccb15
Owolabi Victor <vowolabi16@gmail.com>
2026-08-19 17:03:02 +0100

    Ehanced page loader animation

--- script.js
+++ script.js
@@ -1,26 +1,3 @@
-// Loader Animation
-window.addEventListener("load", () => {
-  const loader = document.getElementById("loader");
-  const spinner = document.getElementById("spinner");
-  const chescaLoader = document.querySelector(".chesca-loader");
-
-  // Show CHESCA after the spinner
-  setTimeout(() => {
-    spinner.style.display = "none";
-    chescaLoader.style.display = "block";
-  }, 700);
-
-  // Start fading out
-  setTimeout(() => {
-    loader.style.animation = "fade-out 1s ease forwards";
-  }, 1700);
-
-  // Completely remove loader
-  setTimeout(() => {
-    loader.style.display = "none";
-  }, 2000);
-});
-
 // Hamburger Menu JS
 const hamburger = document.querySelector(".hamburger");
 const navList = document.querySelector(".nav-list");
